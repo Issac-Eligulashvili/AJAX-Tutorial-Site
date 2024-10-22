6d533d593c9c24e22179728bd1a8d446227785a9
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-10-22 14:26:51 -0400

    done

--- script.js
+++ script.js
@@ -10,13 +10,37 @@ btn.addEventListener("click", function () {
      }
 
      ourRequest.send();
+     pageCounter++;
+     if (pageCounter > 3) {
+          btn.classList.add("hide-me");
+     }
 });
 
 function renderHTML(data) {
      let htmlString = ''
 
      for (i = 0; i < data.length; i++) {
-          htmlString += "<p>" + data[i].name + "is a " + data[i].species + "that "
+          htmlString += "<p>" + data[i].name + "is a " + data[i].species + "that likes to eat ";
+
+          for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+               if (ii === 0) {
+                    htmlString += data[i].foods.likes[ii];
+               } else {
+                    htmlString += " and " + data[i].foods.likes[ii];
+               }
+          }
+
+          htmlString += " and dislikes ";
+
+          for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+               if (ii === 0) {
+                    htmlString += data[i].foods.dislikes[ii]
+               } else {
+                    htmlString += " and " + data[i].foods.dislikes[ii];
+               }
+          }
+
+          htmlString += "</p>"
      }
      animalContainer.insertAdjacentHTML("beforeend", htmlString);
 }
